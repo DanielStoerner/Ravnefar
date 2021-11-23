0ef1674fa050d2acde2dd49d4b27dc71bd12aa72
Daniel Størner <danielstoerner@gmail.com>
2021-11-23 02:07:33 +0100

    nyt

--- Responsive Portfolio Website/script.js
+++ Responsive Portfolio Website/script.js
@@ -31,9 +31,5 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
-    });
-
-    
-
-    
+    });  
 });
